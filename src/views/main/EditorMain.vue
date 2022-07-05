c3a38d728ce17b2e31f9e411e5ba2ad87b7b032b
pm <xuemeng0318@163.com>
2022-07-05 15:13:54 +0800

    init

---
+++ src/views/main/EditorMain.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, reactive, toRefs, onBeforeMount, onMounted } from 'vue'
+// onBeforeMount(() => { })
+// onMounted(() => { })
+</script>
+<template>
+  <header>
+    <div class="title">Animation</div>
+    <div class="toolbar"></div>
+  </header>
+  <div class="content">
+    <div class="setting-box"></div>
+    <div class="show-box"></div>
+  </div>
+</template>
+<style lang='less' scoped>
+</style>
